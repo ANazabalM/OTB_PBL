b67060101fdf96f243d1b40ae809bd7c0783d14a
ibaiig <123636421+ibaiig@users.noreply.github.com>
2024-05-24 16:11:33 +0200

    CambioNobreControlador

--- OTB/src/main/java/com/registro/usuarios/controlador/LoginControlador.java
+++ OTB/src/main/java/com/registro/usuarios/controlador/LoginControlador.java
@@ -1,18 +1,14 @@
 package com.registro.usuarios.controlador;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.registro.usuarios.servicio.UsuarioServicio;
 
 @Controller
-public class RegistroControlador {
+public class LoginControlador {
 
-	@Autowired
-	private UsuarioServicio servicio;
 	
 	@GetMapping("/login")
 	public String iniciarSesion() {
