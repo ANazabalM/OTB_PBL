b3fe297bb49507c4224b84ceea1bd8c3b2c58f3a
ibaiig <123636421+ibaiig@users.noreply.github.com>
2024-06-03 16:21:25 +0200

    Implementación de categorias a la hora de crear el artículo.

--- OTB/src/main/java/com/registro/usuarios/controlador/ArticuloControlador.java
+++ OTB/src/main/java/com/registro/usuarios/controlador/ArticuloControlador.java
@@ -83,6 +83,7 @@ public class ArticuloControlador {
 
     @GetMapping("/articulo/create")
     private String verFormularioCreacion(Model model){
+        model.addAttribute("categoriasVisualizar", categoriaServicio.cogerTodas());
         return "crear_articulo";
     }
     
